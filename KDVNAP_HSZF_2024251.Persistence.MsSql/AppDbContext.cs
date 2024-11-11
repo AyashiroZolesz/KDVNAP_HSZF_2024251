5e9171e754e29eee0b5ccc9205c2bb82138d9099
acszoltan14@stud.uni-obuda.hu <acszoltan14@stud.uni-obuda.hu>
2024-11-11 16:19:14 +0100

    AppDbContext expanded InMemoryDb + JSON

--- KDVNAP_HSZF_2024251.Persistence.MsSql/AppDbContext.cs
+++ KDVNAP_HSZF_2024251.Persistence.MsSql/AppDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using KDVNAP_HSZF_2024251.Model;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 namespace KDVNAP_HSZF_2024251.Persistence.MsSql
 {
@@ -10,8 +11,19 @@ namespace KDVNAP_HSZF_2024251.Persistence.MsSql
         public DbSet<Department> Departments { get; set; }
         public DbSet<Employee> Employees { get; set; }
 
-        // Base setting for the database, F.e.: set from "outer" code (Console project)
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public AppDbContext()
+        {
+            this.Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured != true)
+            {
+                optionsBuilder.UseInMemoryDatabase("Company").UseLazyLoadingProxies();
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
 
         // Define Conenctions
         protected override void OnModelCreating(ModelBuilder modelBuilder) 
@@ -27,6 +39,44 @@ namespace KDVNAP_HSZF_2024251.Persistence.MsSql
 
             base.OnModelCreating(modelBuilder);
 
+            if (File.Exists("factories.json"))
+            {
+                try
+                {
+                    var factories = JsonConvert.DeserializeObject<Factory[]>(File.ReadAllText("factories.json"));
+                    modelBuilder.Entity<Factory>().HasData(factories);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+
+            if (File.Exists("employees.json"))
+            {
+                try
+                {
+                    var employees = JsonConvert.DeserializeObject<Employee[]>(File.ReadAllText("employees.json"));
+                    modelBuilder.Entity<Employee>().HasData(employees);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+
+            if (File.Exists("departments.json"))
+            {
+                try
+                {
+                    var departments = JsonConvert.DeserializeObject<Department[]>(File.ReadAllText("departments.json"));
+                    modelBuilder.Entity<Department>().HasData(departments);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
         }
     }
 }
